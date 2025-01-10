4626dcba7f3d6c0bd7eab751e5db0891c57a9191
sachin yadav <yadav.sachinsk.7@gmail.com>
2025-01-10 15:08:53 +0530

    Datatype conversion opration

---
+++ 01_basics/01_Datatype.js
@@ -0,0 +1,27 @@
+// let score = "33abc" //print string
+// let score = 33   //print Number
+//let score = undefined
+// let score = null
+
+let score = "Sachin"
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+let valueInnumber = Number(score)
+
+// console.log(typeof valueInnumber);
+
+// console.log(valueInnumber);
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+
+// let isLoggedIn = ""
+
+let isLoggedIn = "Sachin"
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
